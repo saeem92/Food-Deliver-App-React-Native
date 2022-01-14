433ccd6577c726b7d0f1a1f6f6bdb7d9a4b61a71
saeem92 <93969890+saeem92@users.noreply.github.com>
2022-01-14 23:59:57 +0530

    changes

--- App.js
+++ App.js
@@ -4,10 +4,13 @@ import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView> 
+    <SafeAreaView style={{flex:1}}> 
     {/* Safeareaview is used to make sure UI is according to iOS and do not overlap with the statusbar in iOS*/}
-    <View>
-    <Text>Our React native Blank Canvas</Text>
+    <View style = {{padding:16, backgroundColor:"green"}}>
+    <Text>Search</Text>
+    </View>
+    <View style = {{flex:1, padding:16, backgroundColor:"blue"}}>
+    <Text>list</Text>
     </View>
     </SafeAreaView>
   );
