bd3fe35e60faaa8fdc9a4ce988c655b27256a3f5
saeem92 <93969890+saeem92@users.noreply.github.com>
2022-01-26 01:22:25 +0530

    migrated stylesheet to styled-components

--- src/features/restaurants/components/ restaurant-info-card.component.js
+++ src/features/restaurants/components/ restaurant-info-card.component.js
@@ -2,10 +2,34 @@
 // Comment for further details
 // Comment to add more details about this component.
 // In the below code we have created a RestaurantInfo component which is taking a specific restaurant as an object, Then we have created a const which has all the properties that our restaurant going to have it consist name,icon,photos,address,open,rating,close then we have set this const = restaurant
+
 import React from "react";
+import styled from "styled-components/native"
+// Here we are importing styled-components that we installed from terminal.
 import { Text, StyleSheet } from "react-native";
 import { Card, TextInput } from "react-native-paper";
-import { backgroundColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
+
+
+
+const RestaurantCard = styled(Card)`
+background-color: white;
+`;
+// RestaurantCard is a styled-component we created above to style our card component.
+
+
+const RestaurantCardCover = styled(Card.Cover)`
+padding: 20px;
+background-color: white;
+`;
+
+// RestaurantCardCover is a styled-component we created above to style our cardcover component.
+
+const Title = styled.Text`
+padding: 16px;
+color: red;
+`;
+// In the above code const Title we have created a styled componenet to style our title component
+// We were able to create this component using styled-components and it has a special type of string (``)
 
 export const RestaurantInfo = ({ restaurant = {} }) => {
   // Here our RestaurantInfo will take an object {restaurant}. We have to set restaurant to be an empty object else its going to be UNDEFINED and if its undefined our code will break.
@@ -24,26 +48,29 @@ export const RestaurantInfo = ({ restaurant = {} }) => {
   } = restaurant; // This is de structuring Here our restaurant is an object that contains properties that we are taking properties off of restaurant restaurant is the object that contains properties. this is the object that we have mentioned above in export const restaurantInfo
 
   return (
-    <Card elevation={5} style={styles.card}>
+    <RestaurantCard elevation={5}>
+    {/* RestaurantCard is a styled-component we created above to style our card component.*/}
       {/* Here we are using our card component which we have imported from react native paper
         Card component has an elevation of 5 elevations give some shadow area to our card component and make it visible
         styles.card is the styles we have created in our stylesheet */}
-      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       {/* Card cover is mainly used to add Image in the card Here key is the restaurant name which will be display on the bottom. Above we have created card cover which is an image added to the card component on the top of it its the top side of our card component which is the large one 
             we can add image in this cover to display it we have give it a source which will display our image in the card component and we gave it a styles.cover 
             styles.cover has padding of 20 we can give more padding depending on how much space you need.
             we have give photos [0] as this is above we have added an image in an array an the url is the 0th element of that array. */}
-      <Text style={styles.title}>{name}</Text>
+      <Title>{name}</Title>
+      {/* In the above code we have used title as we have created it's component above as a styled-component and using it to style the name of the restaurant like its color size etc.*/}
       {/* In the above code name is the prop we have picked from const created above we have added styling to give more space to the restaurant image and pushed away our restaurant name 
             little further than the image and making it look more good you can adjust it accordingly. */}
-    </Card>
+    </RestaurantCard>
   );
 };
 
-const styles = StyleSheet.create({
-  card: { backgroundColor: "white" },
-  cover: { padding: 20, backgroundColor: "white" },
-  title: { padding: 16 },
-  // We can increase the size of the restaurant name by using Fontsize:
-});
-// Add a comment here if needed.
+// Commented stylesheet as we are using styled-components.
+
+// const styles = StyleSheet.create({
+//   card: { backgroundColor: "white" },
+//   cover: { padding: 20, backgroundColor: "white" },
+//   // We can increase the size of the restaurant name by using Fontsize:
+// });
+// // Add a comment here if needed.
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -4,36 +4,46 @@
 import { StatusBar, StyleSheet, Text, View, SafeAreaView } from "react-native";
 import { Searchbar } from "react-native-paper";
 import React from "react";
+import styled from "styled-components/native";
 import { RestaurantInfo } from "../components/ restaurant-info-card.component";
 
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  margin-top: ${StatusBar.currentHeight}px;
+`;
+// IN the above SafeArea we have created a const component SafeArea to style our SafeArea we are doing by using styled-components that we have imported and installed.
+
+// We have used reference variable here for statusbar.currentHeight as we cannot write it normally because we are writing it in a weird template.
+// NOTE: We do not have to stylesheet when we style our components using styled-components.
+
+const SearchContainer = styled.View`
+  padding: 16px;
+`;
+// IN the above SerachContainer we have created a const component SearchContainer which is a view that holds our searchbar and we have give it some padding to give it some space.
+// NOTE: We do not have to stylesheet when we style our components using styled-components.
+
+const RestaurantListContainer = styled.View`
+  flex: 1;
+  padding: 16px;
+  background-color: white;
+`;
+// IN the above code we have given flex:1 in this view so that it will fill the screen completely and fill stick to the constraints of the screen.
+// IN the above RestaurantListContainer we have created a const component RestaurantListContainer to style our SafeArea we are doing by using styled-components that we have imported and installed.
+// NOTE: We do not have to stylesheet when we style our components using styled-components.
+
 export const RestaurantsScreen = () => (
-  <SafeAreaView style={styles.statusbarfix}>
+  <SafeArea>
     {/* Safeareaview is used to make sure UI is according to iOS and do not overlap with the statusbar in iOS we are using */}
-    <View style={styles.search}>
+    <SearchContainer>
+      {/* The searchcontainer is a view we have created where our searchbar lies we have created a styled-component of this view component above and named it search container. */}
       <Searchbar />
       {/* The search bar is imported from react native paper and is helping us to add a searchbox in our app. */}
-    </View>
-    <View style={styles.list}>
-      {/* IN the above code we have given flex:1 in this view so that it will fill the screen completely and fill stick to the constraints of the screen.*/}
+    </SearchContainer>
+    <RestaurantListContainer>
+      {/* This RestaurantListContainer is our View component which we have styled using styled-component created with a name RestaurantListContainer */}
       <RestaurantInfo />
       {/* The above component is imported from restaurant-info-card.component.js file and will display us the list of restaurants in our app with their names. 
       The RestaurantInfo is displaying our card component here.*/}
-    </View>
-  </SafeAreaView>
+    </RestaurantListContainer>
+  </SafeArea>
 );
-
-const styles = StyleSheet.create({
-  statusbarfix: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-    //  flex:1 in the above is to make sure you always fill the parent
-    // In the above code StatusBar.currentHeight is a android only function which takes care of the height of the status bar across all android devices.
-  },
-
-  search: {
-    padding: 16,
-    backgroundColor: "white",
-  },
-
-  list: { flex: 1, padding: 16, backgroundColor: "white" },
-});
