45089942ee915a68a5c439eee1793cbfb4919d9e
saeem92 <93969890+saeem92@users.noreply.github.com>
2022-02-09 01:18:35 +0530

    opennow svg and restaurant icon

--- src/features/restaurants/components/ restaurant-info-card.component.js
+++ src/features/restaurants/components/ restaurant-info-card.component.js
@@ -3,52 +3,79 @@
 // In the below code we have created a RestaurantInfo component which is taking a specific restaurant as an object, Then we have created a const which has all the properties that our restaurant going to have it consist name,icon,photos,address,open,rating,close then we have set this const = restaurant
 
 import React from "react";
-import styled from "styled-components/native"
+import styled from "styled-components/native";
 // Here we are importing styled-components that we installed from terminal.
-import { Text, StyleSheet } from "react-native";
+import { Text, Image, View } from "react-native";
 import { Card, TextInput } from "react-native-paper";
-import { SvgXml } from 'react-native-svg';
+import { SvgXml } from "react-native-svg";
 // In the above code we are importing svgXml to use svg in our app.
 import star from "../../../../assets/star";
-
+import open from "../../../../assets/open";
 
 const RestaurantCard = styled(Card)`
-background-color: white;
+  background-color: white;
 `;
 // RestaurantCard is a styled-component we created above to style our card component.
 
 const Info = styled.View`
-padding: ${(props) => props.theme.space[3]};
-;`
+  padding: ${(props) => props.theme.space[3]}; ;
+`;
 // Const info is used here to add spacing between restaurant name and the card component or image.
 
 const Rating = styled.View`
-flex-direction: row;
-padding-top: ${(props) => props.theme.space[2]};
-padding-bottom: ${(props) => props.theme.space[2]};
-;`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]}; ;
+`;
 // Our stars were aligning underneath each other because flex is default to column so,
 // We have created a styled-component above and changed flex to row from column.
 // Here padding is adding decent amount of space between the rating and the address and the name of the restaurant..
 
 const RestaurantCardCover = styled(Card.Cover)`
-padding: ${(props) => props.theme.space[3]};
-{/* In the above and below code we are using props from infrastructure folder and we are doing theming here. */}
-background-color: ${(props) => props.theme.colors.bg.primary};
+  padding: ${(props) => props.theme.space[3]};
+   {
+    /* In the above and below code we are using props from infrastructure folder and we are doing theming here. */
+  }
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 // RestaurantCardCover is a styled-component we created above to style our cardcover component.
 
 const Address = styled(Text)`
-font-family: ${(props) => props.theme.fonts.body};
-font-size: ${(props) => props.theme.fontSizes.caption}; 
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
 `;
 // The above component is a styled-component used to style address of the restaurant.
 
+const Section = styled.View`
+  flex-direction: row; {/* Here this flex direction is helping us to arrange our stars and open now in a row style else they will appear at center 
+  and both our stars and opennow svg will lie beneath each other in column style.*/}
+  align-items: center;
+`; 
+// The above is a Section styled-component this is helping us to center our opennow svg.
+// Here row means that its going to put content inside in a row style syntax
+// And align-items to the center of the card cover which has name rating and address of the restaurant,.
+
+
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+// The above is a SectionEnd styled-component this is helping us to center our opennow svg.
+// Here our SectionEnd is doing the same thing its going to put content inside in a row style syntax
+// The end outcome of this is that its going to push our opennow svg to the side
+// Flex:1 is helping us to push our opennow svg to the side.
+
+
+
+
 const Title = styled(Text)`
-font-family: ${(props) => props.theme.fonts.heading}; 
-{/* The above code is helping us changing the font of the text which is the name of the restaurant*/}
-font-size: ${(props) => props.theme.fontSizes.body};
-color: ${(props) => props.theme.colors.ui.primary};
+  font-family: ${(props) => props.theme.fonts.heading};
+   {
+    /* The above code is helping us changing the font of the text which is the name of the restaurant*/
+  }
+  font-size: ${(props) => props.theme.fontSizes.body};
+  color: ${(props) => props.theme.colors.ui.primary};
 `;
 // ${props => props.theme.color.ui.primary we have created it using theming that we have created inside infrastructure through theming.}
 // In the above code const Title we have created a styled componenet to style our title component
@@ -59,26 +86,24 @@ export const RestaurantInfo = ({ restaurant = {} }) => {
   // Below code is building some mock information for us to set the scene and start building out our card component.
   const {
     name = "Some Restaurant",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
     ],
     address = "100 some random street",
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily, // All of this information is going to map to information that we are going to be able to get back from our google API once we integrate that later.
+    isClosedTemporarily = true, // All of this information is going to map to information that we are going to be able to get back from our google API once we integrate that later.
     // We are trying to determine the shape of the object we are trying to get
   } = restaurant; // This is de structuring Here our restaurant is an object that contains properties that we are taking properties off of restaurant restaurant is the object that contains properties. this is the object that we have mentioned above in export const restaurantInfo
 
-
-const ratingArray = Array.from(new Array(Math.floor(rating))); 
-// This component is pulling out rating from our const above and is helping us display stars as per restaurant rating.
-// Math.floor is helping us to make sure when we have fraction we will deal that fraction in a whole number we will round of that fraction for example if its 3.2 we will call it 3.
-
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+  // This component is pulling out rating from our const above and is helping us display stars as per restaurant rating.
+  // Math.floor is helping us to make sure when we have fraction we will deal that fraction in a whole number we will round of that fraction for example if its 3.2 we will call it 3.
 
   return (
     <RestaurantCard elevation={5}>
-    {/* RestaurantCard is a styled-component we created above to style our card component.*/}
+      {/* RestaurantCard is a styled-component we created above to style our card component.*/}
       {/* Here we are using our card component which we have imported from react native paper
         Card component has an elevation of 5 elevations give some shadow area to our card component and make it visible
         styles.card is the styles we have created in our stylesheet */}
@@ -88,28 +113,49 @@ const ratingArray = Array.from(new Array(Math.floor(rating)));
             styles.cover has padding of 20 we can give more padding depending on how much space you need.
             we have give photos [0] as this is above we have added an image in an array an the url is the 0th element of that array. */}
       <Info>
-      <Title>{name}</Title>
-      {/* In the above code we have used title as we have created it's component above as a styled-component and using it to style the name of the restaurant like its color size etc.*/}
-      {/* In the above code name is the prop we have picked from const created above we have added styling to give more space to the restaurant image and pushed away our restaurant name 
+        <Title>{name}</Title>
+        {/* In the above code we have used title as we have created it's component above as a styled-component and using it to style the name of the restaurant like its color size etc.*/}
+        {/* In the above code name is the prop we have picked from const created above we have added styling to give more space to the restaurant image and pushed away our restaurant name 
             little further than the image and making it look more good you can adjust it accordingly. */}
-    <Rating>
-    {ratingArray.map(() => (
-    <SvgXml xml={star} width={20} height={20}/>
-    ))}
-    {/* In the above code SvgXml is helping us to use star.js which is a svg file to use it in our app.
+        <Section>
+          {/* We have a section here which is wrapping the entirety of the rating and everything around it
+    it contains rating and it contains sectionend which is a special type of component
+    To sumup this is acting as a view that has some style which is helping us to put our opennow svg at the corner of the card component.*/}
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+            {/* In the above code SvgXml is helping us to use star.js which is a svg file to use it in our app.
     xmo={star} we are using it from star.js file which we have imported above in this file
     We have set the width to 20 and height to 20 to get it fit inside our restaurant card component.
     This is helping us in rating the restaurant by displaying stars.*/}
-    </Rating>
-    {/* The Rating styled-component is used as a view component here for our stars we have placed our ratingarray.map inside it so our stars get aligned in a row and not column which is default before this our stars were aligning underneath each other. */}
-    <Address>{address}</Address>
-    </Info>
+          </Rating>
+          {/* The Rating styled-component is used as a view component here for our stars we have placed our ratingarray.map inside it so our stars get aligned in a row and not column which is default before this our stars were aligning underneath each other. */}
+          <SectionEnd>
+            {/* SectionEnd is a special type of component we have created here to move our opennow svg in the corner of the card component*/}
+            {isClosedTemporarily && (
+              <Text variant= "label" style={{color: "red"}}>
+              CLOSED TEMPORARILY
+              </Text>
+            )} 
+            {/* This is a conditonal if you restaurant is closed its going to show closedrestaurant text */}
+            <View style={{paddingLeft: 16}}/>
+          {/* View style is giving padding between CLOSED TEMPORARILY text and icon. */}
+
+            {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            {/* This is a conditonal if you are open now its going to show you an opennow svg */}
+            <View style={{paddingLeft: 16}}/>
+            {/* View style is giving padding between opennow svg and icon. */}
+            <Image style={{width:15, height:15}} source={{uri: icon}} />
+          {/* Here our image style is taking image from our icon and display when closed temporarily condition is true.
+          icon image is fetched from the url we have assigned in the icon.*/}
+          </SectionEnd>
+        </Section>
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
-
-
-
 
 // Commented stylesheet as we are using styled-components.
 
