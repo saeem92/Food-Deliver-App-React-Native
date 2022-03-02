d997c1ac5f22e180880f66d0d580fcb9be04a45e
saeem92 <93969890+saeem92@users.noreply.github.com>
2022-03-03 01:22:30 +0530

    stack navigation

--- App.js
+++ App.js
@@ -13,48 +13,12 @@ import {LocationContextProvider} from "./src/services/location/location.context"
 import {useFonts as useOswald, Oswald_400Regular,} from '@expo-google-fonts/oswald';
 // The above and below code is importing google fonts that we are using in our app.
 import { useFonts as useLato, Lato_400Regular,} from '@expo-google-fonts/lato';
-
+import { Navigation } from "./src/infrastructure/navigation";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { SafeArea } from "./src/components/safe-area.component";
 import { Ionicons } from "@expo/vector-icons";
 // Ionicons are pre available icons that are came with expoinit that we can use are in our app we are using it to add icons in our bottom navigation of the app.
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
-
-const Tab = createBottomTabNavigator();
-// tab component is used to create the bottom navigation of the app.
-
-const TAB_ICON = {
-  Restaurants: "md-restaurant",
-  Map: "md-map",
-  Settings: "md-settings"
-
-}
-{/* In the above card key is the name of the root*/}
-
-
-const Settings = () =>( 
-    <SafeArea>
-    <Text>Settings</Text>
-    </SafeArea>
-);
-// We have created this settings component here so that we can add this in our bottom navigation inside the app.
-
-
-const Map = () => (
-<SafeArea>
-    <Text>Map</Text>
-    </SafeArea>
-);
-// We have created this Map settings component here so that we can add this in our bottom navigation inside the app
-
-const createScreenOptions = ({ route }) => {
-  const iconName = TAB_ICON[route.name]
-  return{
-    tabBarIcon: ({size,color}) => (
-      <Ionicons name={iconName} size={size} color={color} />
-    ),
-  };
-}; 
 
 
 export default function App() {
@@ -83,21 +47,7 @@ if(!oswaldLoaded || !latoLoaded){
     <LocationContextProvider>
     <RestaurantsContextProvider>
     {/* We are putting the restaurantContextProvider here to give all access of mock data of restaurants here to diplay them.*/}
-      <NavigationContainer>
-      <Tab.Navigator
-            screenOptions={createScreenOptions}
-            tabBarOptions={{
-              activeTintColor: "tomato",
-              inactiveTintColor: "gray",
-            }}
-          >
-          {/* THE ABOVE CODE IS HELPING TO ADD ICONS IN OUR BOTTON NAVIGATION */}
-
-      <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
-            <Tab.Screen name="Map" component={Map} />
-            <Tab.Screen name="Settings" component={Settings} />
-      </Tab.Navigator>
-      </NavigationContainer>
+     <Navigation />
       {/* The above component is created to create bottom navigation in our screen and to also specific component inside our app.
       The NavigationContainer is responsible for managing your app state and linking your top-level navigator to the app environment.
       Tab navigation is Possibly the most common style of navigation in mobile apps is tab-based navigation. This can be tabs on the bottom of the screen or on the top below the header (or even instead of a header basically it is helping us to create botton navigation).
